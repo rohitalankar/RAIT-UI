c4832308a6c78913cca57cd650ae5d2dab01ea0f
rohitalankar <42713446+rohitalankar@users.noreply.github.com>
2019-03-17 11:56:54 +0530

    Add files via upload

---
+++ divc.php
@@ -0,0 +1,168 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+    <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+    <link href="bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css" rel="stylesheet">
+    <link href="bower_components/datatables-responsive/css/dataTables.responsive.css" rel="stylesheet">
+    <link href="dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    
+    <style type="text/css">
+        .table-fixed thead {
+  width: 97%;
+}
+.table-fixed tbody {
+  height: 230px;
+  overflow-y: auto;
+  width: 100%;
+}
+.table-fixed thead, .table-fixed tbody, .table-fixed tr, .table-fixed td, .table-fixed th {
+  display: block;
+}
+.table-fixed tbody td, .table-fixed thead > tr> th {
+  float: left;
+  border-bottom-width: 0;
+}
+    </style>
+    
+
+    
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+      
+     <?php include('leftbar.php');?>
+
+           
+         <nav>
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                   <h4 class="page-header" align="center"> COMPUTER GRAPHICS</h4>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+       
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                            <nav class="navbar navbar-default">
+  <div class="container-fluid">
+
+    <ul class="nav navbar-nav navbar-center" >
+      <li align="center"  style="width:395px;"><a href="index.php">Division A</a></li>
+      <li align="center"  style="width:395px;"><a href="divb.php">Division B</a></li>
+      <li align="center" class="active" style="width:395px;"><a href="divc.php">Division C</a></li>
+    </ul>
+   
+  </div>
+</nav>
+                        <div class="panel-heading">
+                            Lecture Date : 12/3/19<span style="float:right;">Time: 12:30 PM to 4:30 PM</span>
+                            
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class=" col-lg-9">
+                         <table class="table table-fixed">
+          <thead>
+            <tr>
+              <th class="col-xs-5">Name</th><th class="col-xs-3">Roll No</th><th class="col-xs-3">Marks</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="col-xs-5">RAIT</td><td class="col-xs-3">12</td><td class="col-xs-3">23</td>
+            </tr>
+             <tr>
+              <td class="col-xs-5">RAIT</td><td class="col-xs-3">12</td><td class="col-xs-3">23</td>
+            </tr>
+             <tr>
+              <td class="col-xs-5">RAIT</td><td class="col-xs-3">12</td><td class="col-xs-3">23</td>
+            </tr>
+             <tr>
+              <td class="col-xs-5">RAIT</td><td class="col-xs-3">12</td><td class="col-xs-3">23</td>
+            </tr>
+             <tr>
+              <td class="col-xs-5">RAIT</td><td class="col-xs-3">12</td><td class="col-xs-3">23</td>
+            </tr>
+             <tr>
+              <td class="col-xs-5">RAIT</td><td class="col-xs-3">12</td><td class="col-xs-3">23</td>
+            </tr>
+             <tr>
+              <td class="col-xs-5">RAIT</td><td class="col-xs-3">12</td><td class="col-xs-3">23</td>
+            </tr>
+             <tr>
+              <td class="col-xs-5">RAIT</td><td class="col-xs-3">12</td><td class="col-xs-3">23</td>
+            </tr>
+             <tr>
+              <td class="col-xs-5">RAIT</td><td class="col-xs-3">12</td><td class="col-xs-3">23</td>
+            </tr>
+             <tr>
+              <td class="col-xs-5">RAIT</td><td class="col-xs-3">12</td><td class="col-xs-3">23</td>
+            </tr>
+           
+          </tbody>
+        </table>
+                            </div>
+                            <div class="form-group  col-lg-3">
+                                <div class="well" style="height:250px;">
+                                <div>
+                                    <label for="files" class="btn text-center" style="margin:30px; padding-top:50px;"><h3>Upload Photo</h3></label>
+                                      <input id="files" style="visibility:hidden;" type="file" name="photo">
+                                </div>
+                                
+                            </div>
+                            <button class="btn btn-primary" name="conducted" style="width:100%;">CONDUCTED</button> 
+                                
+                            </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+           
+            
+           
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- DataTables JavaScript -->
+    <script src="bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
+    <script src="bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js"></script>
+
+   
+
+</body>
+
+</html>
---
+++ leftbar.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-default navbar-static-top" role="navigation"
+			style="margin-bottom: 0;margin-top:2%">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse"
+					data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span> <span
+						class="icon-bar"></span> <span class="icon-bar"></span> <span
+						class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="index.php"><img src="logo1.png" style="margin-top:-5%; width:100%;"></a>
+			</div>
+						 <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        
+                 <li>
+                 <a href="#"> lorem ipsum</a>
+                </li>
+                        
+                   <li>
+                            <a href="#"> lorem ipsum</a>
+                  </li>     
+                                            
+                   <li>
+                            <a href="">lorem ipsum</a>
+                  </li>      
+                   <li>
+                            <a href="index.php"> Remedial</a>
+                        </li>
+                  </ul>      				  
+                </div>
+               
+            </div>
+            
+        </nav>
